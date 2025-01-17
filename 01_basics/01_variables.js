289316c29d5eef60d1727c90041e68f260d9ca54
dronaop <96990829+dronaop@users.noreply.github.com>
2025-01-17 12:28:47 +0000

    02

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountID = 107 //Immutable
+let accountEmail = "Deepanshudrona@gmail.com"
+var accountPassword = "1234"
+accountCity = "Delhi"
+let accountState
+accountEmail ="DD@gmail.com"
+
+console.log(accountID)
+
+/*
+    Prefer not to use var because 
+    of issues in block scope and functional scope
+
+*/
+
+console.table([accountID, accountEmail,accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict "; // treat all JS code as newer version 
+
+// alert (3 + 3) we are using nodejs , not browser
+// documentation TC39 and MDN
+
+console.log(3 + 3 )
+let name = "Deepanshu"
+let age = 23
+let isloggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string
+// Bollean => true/false
+// null => standalone value
+// undefined => not assigned value
+// symbol => unique
+
+// object 
+console.log(typeof name)
+console.log(typeof null)
